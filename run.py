a03b006181e95f3da232009d47125daa52eed365
Chris Hedrick <christopher.m.hedrick@gmail.com>
2013-11-25 14:49:24 -0500

    added user auth through cookie

--- run.py
+++ run.py
@@ -4,13 +4,23 @@ import sys
 import sqlite3
 import datetime
 from crypt import pwd_context
-from bottle import route, run, template, static_file, request, post
+from bottle import route, run, template, static_file, request, post, response
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(current_dir)
 sys.path.append(current_dir)
 conn = sqlite3.connect("database.db")
 cur = conn.cursor()
+
+def check_login():
+    key = "$pbkdf2-sha256$7376$u/c.JwQAQKiVcq71ntM6Rw$zQlWEpB6WMpS8oEKUrVs1oSDDdYMiwjRet5XISbYcm8"
+    try:
+        if pwd_context.verify(request.get_cookie('cookie'), key) == True:
+            return True
+        else:
+            return False
+    except:
+        return False
 
 @route('/static/<filename:path>')
 def server_static(filename):
@@ -20,7 +30,11 @@ def server_static(filename):
 @route('/')
 def index():
     cur.execute('select * from users')
-    return template('templates/login.tpl') 
+    if check_login() == True:
+        return manage_people()
+    else:
+        return template('templates/login.tpl')
+
 
 @post('/login')
 def verify():
@@ -30,6 +44,7 @@ def verify():
         cmd = "select password from users where username='%s'" % (uname)
         password = cur.execute(cmd).fetchone()[0]
         if pwd_context.verify(form['password'],password) == True:
+            response.set_cookie('cookie', "loggedin")
             return manage_people()
         else:
             return index()
@@ -39,54 +54,77 @@ def verify():
 @route('/persons')
 def manage_people():
     people = cur.execute('select * from persons').fetchall()
-    form_ext = request.fullpath.split('/')[1]
-    return template('templates/people.tpl', people=people, form_ext=form_ext)
+    if check_login() == True:
+        form_ext = request.fullpath.split('/')[1]
+        return template('templates/people.tpl', people=people, form_ext=form_ext)
+    else:
+        return index()
 
+@route('/loans')
+def manage_loans():
+    loaned = cur.execute('select * from loans').fetchall()
+    if check_login() == True:
+        form_ext = request.fullpath.split('/')[1]
+        return template('templates/loans.tpl', loaned=loaned, form_ext=form_ext)
+    else:
+        return index()
 
 @route('/items')
 def manage_items():
     items = cur.execute('select * from items').fetchall()
-    form_ext = request.fullpath.split('/')[1]
-    return template('templates/items.tpl', items=items, form_ext=form_ext)
+    if check_login() == True:
+        form_ext = request.fullpath.split('/')[1]
+        return template('templates/items.tpl', items=items, form_ext=form_ext)
+    else:
+        return index()
 
 @route('/add_form/<table>')
 def add_form(table):
-    column_list = []
-    for columns in cur.description:
-        column_list.append(columns[0])
-    return template('templates/addform.tpl', column_list=column_list, table=table)
+    if check_login() == True:
+        column_list = []
+        for columns in cur.description:
+            column_list.append(columns[0])
+        return template('templates/addform.tpl', column_list=column_list, table=table)
+    else:
+        return index()
 
 @route('/add_form/loans/<pid>')
 def add_loan(pid):
-    column_list = []
-    cur.execute("select * from loans")
-    for columns in cur.description:
-        column_list.append(columns[0])
-    cmd = 'select * from persons where id=%s' % (pid)
-    person_data = cur.execute(cmd).fetchone()
-    person_known = dict(zip(column_list, person_data))
-    tdt = datetime.date.today()
-    return template('templates/addloan.tpl', column_list=column_list, table='loans', person=person_known, tdt=tdt)
+    if check_login() == True:
+        column_list = []
+        cur.execute("select * from loans")
+        for columns in cur.description:
+            column_list.append(columns[0])
+        cmd = 'select * from persons where id=%s' % (pid)
+        person_data = cur.execute(cmd).fetchone()
+        person_known = dict(zip(column_list, person_data))
+        tdt = datetime.date.today()
+        return template('templates/addloan.tpl', column_list=column_list, table='loans', person=person_known, tdt=tdt)
+    else:
+        return index()
  
-
 @post('/added/<table>')
 def added(table):
-    try:
-        ext_data = []
-        ext_data.append(table)
-        if table == "loans":
-            cur.execute("select * from loans")
-        column_data = request.forms
-        for key in cur.description:
-            ext_data.append(column_data[key[0]])
-        col_placeholder = "'%s '," * len(column_data.keys())
-        cmd = ("insert into %s values (" + col_placeholder[:-1] + ')') % tuple(ext_data)
-        cur.execute(cmd)
-        conn.commit()
-        cur.close
-    except:
-        print("error")
-    return index()
+    if check_login() == True:
+        try:
+            ext_data = []
+            ext_data.append(table)
+            if table == "loans":
+                cur.execute("select * from loans")
+            column_data = request.forms
+            for key in cur.description:
+                ext_data.append(column_data[key[0]])
+            col_placeholder = "'%s '," * len(column_data.keys())
+            cmd = ("insert into %s values (" + col_placeholder[:-1] + ')') % tuple(ext_data)
+            cur.execute(cmd)
+            conn.commit()
+            cur.close
+            return index()
+        except:
+            print("error")
+            return index()
+    else:
+        return index()
 
 if __name__ == '__main__':
     run(host='localhost', port=8000, debug=True)
---
+++ templates/loans.tpl
@@ -0,0 +1,20 @@
+<table>
+  <tr>
+    <th>Person ID</th>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Item ID</th>
+    <th>Item</th>
+    <th>Date Checked Out</th>
+  </tr>
+  %for row in loaned:
+  <tr>
+    %for col in row:
+    <td>{{col}}</td>
+    %end
+ </tr>
+  %end
+</table>
+
+
+%rebase templates/base.tpl title='loans'
